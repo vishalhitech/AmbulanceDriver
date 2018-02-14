b1ccdceff13e712c79cc3a848ae755a6121c1175
Vishal <vishalcseng@gmail.com>
2018-02-14 14:39:24 +0530

    basis on/off function working.

--- app/src/main/java/com/example/hp/ambulancedriver/MainActivity.java
+++ app/src/main/java/com/example/hp/ambulancedriver/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.hp.ambulancedriver;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,13 +16,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = (Button) findViewById(R.id.button);
+        final Button btn = (Button) findViewById(R.id.btn);
+        final EditText deviceId = (EditText) findViewById(R.id.DeviceID);
+
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent FirstPageIndent = new Intent(MainActivity.this,InfoActivity.class);
-                startActivity(FirstPageIndent);
+
+                String btnText = (String) btn.getText();
+
+                if(btnText == "ON") {
+                    deviceId.setEnabled(false);
+                    btn.setText("OFF");
+
+
+                }
+                else {
+                    deviceId.setEnabled(true);
+                    deviceId.setText("");
+                    deviceId.setHint("Enter Your ID : ");
+                    btn.setText("ON");
+
+
+
+
+                }
+
+
+
             }
         });
 
